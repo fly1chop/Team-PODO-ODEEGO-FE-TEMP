b0fdb768fa6d41acefd27239cff607c1c80109a7
Seung-hwan285 <ghks123285@gmail.com>
2023-02-09 17:37:59 +0900

    chore: cypress test

---
+++ cypress/e2e/1-getting-started/app.cy.js
@@ -0,0 +1,9 @@
+describe('Navgation',()=>{
+   it('네비게이션 페이지 테스트',()=>{
+       cy.visit('http://localhost:3000/');
+       cy.get('a[href*="/pages/About"]').click()
+
+       cy.url().should('include','/About');
+       cy.get('h1').contains('AboutPage');
+   })
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@
 
 import styles from './page.module.css';
 import Todo from "@/app/components/todo";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Todo/>
+        <h1>Homepage</h1>
+        {/*Cypress Test*/}
+        <Link href="/pages/About">About</Link>
     </main>
   );
 }
