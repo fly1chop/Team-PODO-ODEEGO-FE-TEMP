dc124c033290186738fdfcf49dd184554261de02
Seung-hwan285 <ghks123285@gmail.com>
2023-02-09 19:16:51 +0900

    style: init style (normalize)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactQueryWrapper from '@/app/ReactQueryWrapper';
 import Recoil from '@/app/Recoil';
+import './globals.css';
 
 export default function RootLayout({
   children,
@@ -9,10 +10,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head /> will contain the components returned by the nearest parent
-      head.tsx. Find out more at
-      https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      <head />
       <body>
         <Recoil>
           <ReactQueryWrapper>{children}</ReactQueryWrapper>
